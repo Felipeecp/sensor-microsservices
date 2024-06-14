8bdaf978ef4f96633b40e08965770f3590d9cfb0
Felipeecp <felipe.engcp@gmail.com>
2024-06-14 16:24:05 -0300

    cria serviço de coleta de dados

---
+++ SensorColetaDeDados/src/main/kotlin/io/github/felipeecp/sensorcoletadedados/SensorColetaDeDadosApplication.kt
@@ -0,0 +1,11 @@
+package io.github.felipeecp.sensorcoletadedados
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SensorColetaDeDadosApplication
+
+fun main(args: Array<String>) {
+	runApplication<SensorColetaDeDadosApplication>(*args)
+}
---
+++ SensorColetaDeDados/src/test/kotlin/io/github/felipeecp/sensorcoletadedados/SensorColetaDeDadosApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.felipeecp.sensorcoletadedados
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SensorColetaDeDadosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
